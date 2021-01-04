6814da5ab517cbb76a054659d6d41ee92187da01
‘mxg133’ <396330646@qq.com>
2021-01-04 16:11:21 +0800

    127

--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -3,10 +3,23 @@ package com.atguigu.gulimall.search;
 import com.alibaba.fastjson.JSON;
 import com.atguigu.gulimall.search.config.GulimallElasticSearchConfig;
 import lombok.Data;
+import lombok.ToString;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Avg;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +34,83 @@ public class GulimallSearchApplicationTests {
 
     @Autowired
     RestHighLevelClient client;
+
+    @ToString
+    @Data
+    static class Account {
+
+        private int account_number;
+        private int balance;
+        private String firstname;
+        private String lastname;
+        private int age;
+        private String gender;
+        private String address;
+        private String employer;
+        private String email;
+        private String city;
+        private String state;
+    }
+
+    @Test
+    public void searchData() throws IOException {
+        //1 创建检索请求
+        SearchRequest searchRequest = new SearchRequest();
+        //指定索引
+        searchRequest.indices("bank");
+        //指定DSL（检索条件） sourceBuilder封装的条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        //1.1 构造检索条件
+//        sourceBuilder.query();
+//        sourceBuilder.from();
+//        sourceBuilder.size();
+//        sourceBuilder.aggregation();
+        sourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+
+        //1.2 按照年龄的值分布进行聚合
+        TermsAggregationBuilder ageAgg = AggregationBuilders.terms("ageAgg").field("age").size(10);
+        sourceBuilder.aggregation(ageAgg);
+
+        //1.3 计算平均薪资
+        AvgAggregationBuilder balanceAvg = AggregationBuilders.avg("balanceAvg").field("balance");
+        sourceBuilder.aggregation(balanceAvg);
+
+        System.out.println("打印检索条件-> " + sourceBuilder.toString());
+
+        searchRequest.source(sourceBuilder);
+
+        //2 执行检索
+        SearchResponse searchResponse = client.search(searchRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+
+        //3 分析结果 searchResponse
+        System.out.println("打印分析结果-> " + searchResponse.toString());
+//        Map map = JSON.parseObject(searchResponse.toString(), Map.class);
+        //3.1 获取所有查到的数据
+        SearchHits hits = searchResponse.getHits();
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit hit : searchHits) {
+//            hit.getIndex();
+//            hit.getType();
+//            hit.getId();
+            String string = hit.getSourceAsString();
+            Account account = JSON.parseObject(string, Account.class);
+            System.out.println(account);
+        }
+
+        //3.2 获取这次检索到的分析信息
+        Aggregations aggregations = searchResponse.getAggregations();
+//        for (Aggregation aggregation : aggregations.asList()) {
+//            System.out.println("聚合的名字-> " + aggregation.getName());
+//            aggregation
+//        }
+        Terms ageAgg1 = aggregations.get("ageAgg");
+        for (Terms.Bucket bucket : ageAgg1.getBuckets()) {
+            String keyAsString = bucket.getKeyAsString();
+            System.out.println("年龄：" + keyAsString + "->" + bucket.getDocCount());
+        }
+        Avg balanceAvg1 = aggregations.get("balanceAvg");
+        System.out.println("平均薪资:" + balanceAvg1.getValue() + "日元");
+    }
 
     /**
      * 测试存储数据到es
