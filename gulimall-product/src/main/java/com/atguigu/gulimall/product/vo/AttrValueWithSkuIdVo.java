74ccc264dc942f17066caa9f4bb81e0e95d9785f
‘mxg133’ <396330646@qq.com>
2021-01-27 15:52:57 +0800

    208 商城业务-商品详情-销售属性渲染

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/AttrValueWithSkuIdVo.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author 孟享广
+ * @date 2021-01-27 2:26 下午
+ * @description
+ */
+@Data
+public class AttrValueWithSkuIdVo {
+
+    private String attrValue;
+    private String skuIds;
+}
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/SkuItemSaleAttrVo.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/SkuItemSaleAttrVo.java
@@ -15,5 +15,5 @@ import java.util.List;
 public class SkuItemSaleAttrVo {
     private Long attrId;
     private String attrName;
-    private String attrValues;
+    private List<AttrValueWithSkuIdVo> attrValues;
 }
