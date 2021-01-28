118ffc539f896c26cbacb30aa88b888fa5d589ab
‘mxg133’ <396330646@qq.com>
2021-01-28 11:04:47 +0800

    211 商城业务-认证服务-环境搭建

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.gulimall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+//开启feign客户度的远程调用功能gulimall-auth-server
+//扫描feign文件夹下的带有@FeignClient注解的接口
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.auth.feign")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallAuthServerApplication.class, args);
+    }
+
+}
