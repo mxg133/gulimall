df60cd9bef0a25003fc8f266db56d699f68411d2
‘mxg133’ <396330646@qq.com>
2021-02-03 13:29:42 +0800

    238 商城业务-购物车-VO编写

---
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/vo/Cart.java
@@ -0,0 +1,78 @@
+package com.atguigu.gulimall.cart.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @author 孟享广
+ * @date 2021-02-03 12:33 下午
+ * @description 整个购物车
+ * @Data 不用的原因
+ *  需要计算的属性必须重新get()的方法，这样才能保证每一次属性都会进行重新计算
+ */
+//@Data
+public class Cart {
+
+    List<CartItem> items;
+    //全部sku的总数 3+3=6
+    private Integer countNum;
+    //共有多少种类型
+    private Integer countType;
+    //所有sku总价
+    private BigDecimal totalAmount;
+    //优惠价格
+    private BigDecimal reduce = new BigDecimal("0.00");
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public Integer getCountNum() {
+
+        int count = 0;
+        if (items != null && items.size() > 0) {
+            for (CartItem item : items) {
+                count += item.getCount();
+            }
+        }
+        return count;
+    }
+
+    public Integer getCountType() {
+
+        int count = 0;
+        if (items != null && items.size() > 0) {
+            for (CartItem item : items) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public BigDecimal getTotalAmount() {
+
+        BigDecimal amount = new BigDecimal("0");
+        //1 计算购物项的总价
+        if (items != null && items.size() > 0) {
+            for (CartItem item : items) {
+                amount = amount.add(item.getTotalPrice());
+            }
+        }
+        //2 减掉优惠总价
+        return amount.subtract(getReduce());
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public BigDecimal getReduce() {
+        return reduce;
+    }
+
+    public void setReduce(BigDecimal reduce) {
+        this.reduce = reduce;
+    }
+}
---
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/vo/CartItem.java
@@ -0,0 +1,99 @@
+package com.atguigu.gulimall.cart.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @author 孟享广
+ * @date 2021-02-03 12:33 下午
+ * @description 购物车里的每一个购物项
+ * @Data 不用的原因
+ *  需要计算的属性必须重新get()的方法，这样才能保证每一次属性都会进行重新计算
+ */
+//为什么不用@Data 因为要自定义一个方法getTotalPrice()
+//@Data
+public class CartItem {
+
+    private Long skuId;
+    //是否被选中
+    private Boolean check = true;
+    private String title;
+    private String image;
+    //套餐信息
+    private List<String> skuAttr;
+    //涉及到计算 必须用BigDecimal
+    private BigDecimal price;
+    private Integer count;
+    //总价
+    private BigDecimal totalPrice;
+
+    public Long getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(Long skuId) {
+        this.skuId = skuId;
+    }
+
+    public Boolean getCheck() {
+        return check;
+    }
+
+    public void setCheck(Boolean check) {
+        this.check = check;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<String> getSkuAttr() {
+        return skuAttr;
+    }
+
+    public void setSkuAttr(List<String> skuAttr) {
+        this.skuAttr = skuAttr;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    /**
+     * 计算每一个购物项的总价
+     */
+    public BigDecimal getTotalPrice() {
+
+        return this.price.multiply(new BigDecimal(this.count+""));
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
