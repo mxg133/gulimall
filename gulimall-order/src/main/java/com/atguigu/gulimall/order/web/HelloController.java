532e076a2e2f17e64de7c0f8dfc59f99130353f7
‘mxg133’ <396330646@qq.com>
2021-02-11 14:18:42 +0800

    293 商城业务-订单服务-延时队列定时关单模拟

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/web/HelloController.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/web/HelloController.java
@@ -1,8 +1,14 @@
 package com.atguigu.gulimall.order.web;
 
+import com.atguigu.gulimall.order.entity.OrderEntity;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 /**
  * @author 孟享广
@@ -11,6 +17,21 @@ import org.springframework.web.bind.annotation.PathVariable;
  */
 @Controller
 public class HelloController {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @ResponseBody
+    @GetMapping("/test/creatPOrder")
+    public String creatPOrderTest() {
+        OrderEntity entity = new OrderEntity();
+        entity.setOrderSn("10010");
+        entity.setModifyTime(new Date());
+
+        //给MQ发消息
+        rabbitTemplate.convertAndSend("order-event-exchange", "order.create.order", entity);
+        return "ok";
+    }
 
     @GetMapping("/{page}.html")
     public String listPage(@PathVariable("page") String page) {
