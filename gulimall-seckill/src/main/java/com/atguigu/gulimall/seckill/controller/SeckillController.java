89e4c722b54e5b411ed678dc6ac1e95f92f725d8
‘mxg133’ <396330646@qq.com>
2021-02-23 17:08:24 +0800

    338 Sleuth-链路追踪-Zipkin界面分析

--- gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/controller/SeckillController.java
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/controller/SeckillController.java
@@ -43,11 +43,11 @@ public class SeckillController {
     @GetMapping("/sku/seckill/{skuId}")
     public R getSkuSeckillInfo(@PathVariable("skuId") Long skuId) {
 
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(300);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         SeckillSkuRedisTo to = seckillService.getSkuSeckillInfo(skuId);
         return R.ok().setData(to);
