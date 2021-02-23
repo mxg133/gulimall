95e5664c362f404bc532173ad218098cd55e6b36
‘mxg133’ <396330646@qq.com>
2021-02-23 17:06:48 +0800

    337 Sleuth-链路追踪-整合Zipkin效果

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -9,6 +9,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *  1 依赖 spring-cloud-starter-sleuth （common）
  *  2 yml logging:level:org.springframework.cloud.openfeign: debug;
  *     （这两个每个服务都应该有） org.springframework.cloud.sleuth: debug
+ *
+ *  整合Zipkin链路追踪可视化展示 （包含sleuth）
+ *  1 依赖 spring-cloud-starter-zipkin（common）
  */
 //注册到注册中心（发现中心）
 @EnableDiscoveryClient
